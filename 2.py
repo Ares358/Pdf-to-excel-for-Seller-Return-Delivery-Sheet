73e361c0408b40edcc95c03f3d6d36d0dc68a0c1
Sunny Kushwaha <52101053+Ares358@users.noreply.github.com>
2023-02-23 08:09:03 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -43,10 +43,10 @@ st.title("PDF Data Extraction")
 
 # File uploader
 file = st.file_uploader("Upload a PDF file", type="pdf")
+print(file)
 file = BytesIO(file.read())
 if file is not None:
     # Extract data and display in a table
-    print(file)
     df = extract_data(file)
     st.write(df)
 
